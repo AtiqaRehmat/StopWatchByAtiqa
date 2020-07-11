89a9fbc95c4bce913bee5a89c4cdd5aeed3adbaf
AtiqaRehmat <atiqakhan288@gmail.com>
2020-07-11 15:39:09 +0500

    update stop watch

--- app.js
+++ app.js
@@ -19,7 +19,7 @@
 
 
 
-var name;
+// var name;
 
 
 
@@ -27,143 +27,18 @@ var name;
 
 
 
-var hr=0;
-var min=0;
-var sec=0;
-var mili=0;
-
-
-var Hours=document.getElementById("hr");
-var Minutes=document.getElementById("min");
-var Seconds=document.getElementById("sec");
-var MiliSeconds=document.getElementById("mili");
-
-
-var interval;
-
-function timer()
-{
-mili++;
-MiliSeconds.innerHTML=mili;
-if(mili>=100)
-{
-sec++;
-Seconds.innerHTML=sec;
-mili=0;
-}
-else if(sec>=60)
-{
-min++;
-Minutes.innerHTML=min;
-sec=0;
-}
-else if(min>=60)
-{
-    hr++;
-Hours.innerHTML=hr;
-min=0;
-}
-}
-interval= setInterval(timer,10)
-
-var name;
-function GetName()
-{
-name=prompt("Enter name ");
-min=0;
-sec=0;
-mili=0;
-hr=0;
-Hours.innerHTML=hr;
-Minutes.innerHTML=min;
-Seconds.innerHTML=sec;
-MiliSeconds.innerHTML=mili;
-interval= setInterval(timer,10)
-}
-
-
-function start()
-{
-   GetName()
-   
-
-
-}
-
-
-
-var reply;
-
-function pause()
-{
-    reply=prompt(name+" sure you stop the watch (Yes Or No) ");
-  
-   if(reply === "yes")
-   {
-       clearInterval(interval);
-   }
-   else
-   {
-       min=0;
-sec=0;
-mili=0;
-hr=0;
-Hours.innerHTML=hr;
-Minutes.innerHTML=min;
-Seconds.innerHTML=sec;
-MiliSeconds.innerHTML=mili;
-interval= setInterval(timer,10)
-   }
-    
-}
-
-
-
-
-
-
-
-var resetAns;
-function reset()
-{
-   resetAns=prompt(" you reset the watch");
-   if(resetAns === "yes")
-   {
-min=0;
-sec=0;
-mili=0;
-hr=0;
-Hours.innerHTML=hr;
-Minutes.innerHTML=min;
-Seconds.innerHTML=sec;
-MiliSeconds.innerHTML=mili;
-clearInterval(interval)
-   }
-   else
-   {
-    clearInterval(interval);  
-   }
-}
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
+// var hr=0;
 // var min=0;
 // var sec=0;
 // var mili=0;
+
+
+// var Hours=document.getElementById("hr");
 // var Minutes=document.getElementById("min");
 // var Seconds=document.getElementById("sec");
 // var MiliSeconds=document.getElementById("mili");
+
+
 // var interval;
 
 // function timer()
@@ -182,28 +57,158 @@ clearInterval(interval)
 // Minutes.innerHTML=min;
 // sec=0;
 // }
+// else if(min>=60)
+// {
+//     hr++;
+// Hours.innerHTML=hr;
+// min=0;
+// }
 // }
 // interval= setInterval(timer,10)
 
-// function start()
+// var name;
+// function GetName()
 // {
-// interval= setInterval(timer,10)
-// }
-// function pause()
-// {
-// clearInterval(interval)
-// }
-// function reset()
-// {
-   
+// name=prompt("Enter name ");
 // min=0;
 // sec=0;
 // mili=0;
+// hr=0;
+// Hours.innerHTML=hr;
 // Minutes.innerHTML=min;
 // Seconds.innerHTML=sec;
 // MiliSeconds.innerHTML=mili;
-// pause()
+// interval= setInterval(timer,10)
 // }
+
+
+// function start()
+// {
+//    GetName()
+   
+
+
+// }
+
+
+
+// // var reply;
+
+// function pause()
+// {
+//    //  prompt(name+" you stop the watch ");
+//     clearInterval(interval);
+// //    if(reply === "yes")
+// //    {
+// //        clearInterval(interval);
+// //    }
+// //    else
+// //    {
+// //        min=0;
+// // sec=0;
+// // mili=0;
+// // hr=0;
+// // Hours.innerHTML=hr;
+// // Minutes.innerHTML=min;
+// // Seconds.innerHTML=sec;
+// // MiliSeconds.innerHTML=mili;
+// // interval= setInterval(timer,10)
+// //    }
+    
+// }
+
+
+
+
+
+
+
+// var resetAns;
+// function reset()
+// {
+//    resetAns=prompt(" you reset the watch");
+//    if(resetAns === "yes")
+//    {
+// min=0;
+// sec=0;
+// mili=0;
+// hr=0;
+// Hours.innerHTML=hr;
+// Minutes.innerHTML=min;
+// Seconds.innerHTML=sec;
+// MiliSeconds.innerHTML=mili;
+// clearInterval(interval)
+//    }
+//    else
+//    {
+//     clearInterval(interval);  
+//    }
+// }
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+var min=0;
+var sec=0;
+var mili=0;
+var Minutes=document.getElementById("min");
+var Seconds=document.getElementById("sec");
+var MiliSeconds=document.getElementById("mili");
+var interval;
+
+function timer()
+{
+mili++;
+MiliSeconds.innerHTML=mili;
+if(mili>=100)
+{
+sec++;
+Seconds.innerHTML=sec;
+mili=0;
+}
+else if(sec>=60)
+{
+min++;
+Minutes.innerHTML=min;
+sec=0;
+}
+}
+interval= setInterval(timer,10)
+
+var name;
+
+function start()
+{
+interval= setInterval(timer,10)
+name=prompt("Enter your name ");
+}
+function pause()
+{
+   
+clearInterval(interval)
+alert(name+" you stop the watch");
+}
+function reset()
+{
+   
+min=0;
+sec=0;
+mili=0;
+Minutes.innerHTML=min;
+Seconds.innerHTML=sec;
+MiliSeconds.innerHTML=mili;
+clearInterval(interval)
+}
 
 
 
